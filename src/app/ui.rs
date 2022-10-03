a6a1390c70cd10b49dbe6b6b31a4335ebb5cb4c5
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-03 10:18:25 +0200

    Isolating UI creation in separate module

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -1,1 +1,11 @@
-    pub fn get_chips(){}
+use cursive::Cursive;
+use cursive::views::{ResizedView, NamedView, SelectView};
+use cursive::view::{Nameable, Resizable};
+
+
+pub fn get_select_view(on_submit: fn(s: &mut Cursive, name: &str)) -> ResizedView<NamedView<SelectView>> {
+     SelectView::<String>::new()
+        .on_submit(on_submit)
+        .with_name("select")
+        .fixed_size((10, 5))
+}
