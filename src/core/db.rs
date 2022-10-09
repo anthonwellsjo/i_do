37148fbedf9e55d17093acb3ff8c89f0f21d33c2
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-09 14:58:54 +0200

    Creating proper schema and implementing CRUD + tests

--- src/core/db.rs
+++ src/core/db.rs
@@ -1,8 +1,30 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-struct ToDo {
-    title: String,
-    descriptions: String,
-    phones: Vec<String>,
+pub struct ToDo<'a> {
+    id: &'a str,
+    description: &'a str,
+    done: bool,
+}
+
+pub fn save_todo(todo: ToDo) {
+    let json = serde_json::to_string(&todo).unwrap_or_else(|_| {
+        panic!("Error, could not covert to JSON");
+    });
+
+
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::core::db::{ToDo, save_todo};
+
+    #[test]
+    fn save_a_todo() {
+        let to_do = ToDo{id: "123", description: "Test todo", done: false};
+        save_todo(to_do);
+    }
 }
