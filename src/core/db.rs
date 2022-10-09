0e4128d14b2ccd6cd68b4ecee33ebffaa406d8cd
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-09 16:10:53 +0200

    Write to file test

--- src/core/db.rs
+++ src/core/db.rs
@@ -1,4 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{ErrorKind, Write},
+};
 
 #[derive(Serialize, Deserialize)]
 pub struct ToDo<'a> {
@@ -12,19 +16,34 @@ pub fn save_todo(todo: ToDo) {
         panic!("Error, could not covert to JSON");
     });
 
+    let file = File::open("db.json").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("db.json").unwrap_or_else(|error| {
+                panic!("Problem creating a new database: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the database: {:?}", error);
+        }
+    });
 
+    std::fs::write(
+        "db.json",
+        serde_json::to_string_pretty(&json).unwrap(),
+    )
+    .unwrap();
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
-    use crate::core::db::{ToDo, save_todo};
+    use crate::core::db::{save_todo, ToDo};
 
     #[test]
     fn save_a_todo() {
-        let to_do = ToDo{id: "123", description: "Test todo", done: false};
+        let to_do = ToDo {
+            id: "123",
+            description: "Test todo",
+            done: false,
+        };
         save_todo(to_do);
     }
 }
