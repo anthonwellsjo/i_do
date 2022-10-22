1a431293a435f25bb6403aca01db7eed9d490312
AnthonWellsjo <anthonwellsjo@gmail.com>
2022-10-22 21:58:48 +0200

    Updating imports

--- src/core/ui.rs
+++ src/core/ui.rs
@@ -4,7 +4,7 @@ use cursive::views::{
 };
 use cursive::Cursive;
 
-use super::db::{self, get_todos, delete_todo_from_db };
+use super::db::{self, delete_todo_from_db, get_todos};
 
 pub fn get_select_view(
     on_submit: fn(s: &mut Cursive, name: &str),
